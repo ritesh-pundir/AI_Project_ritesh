35677797be88af440dc3250b80a3f887240a85e3
riteshpundir <201b215@juetguna.in>
2022-05-28 11:03:22 +0530

    all answer set

--- app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
+++ app/src/main/java/com/example/ai_mad_numgame/MainActivity.java
@@ -72,12 +72,50 @@ public class MainActivity extends AppCompatActivity {
     public void newMatch() {  //A game is composed of three matches
 
         int operand1 = random.nextInt(10);
-        int operand2=0;
+        int operand2= random.nextInt(10);
+        correctButton = random.nextInt(4);
+
         //check is operand2 is not zero; otherwise in case of division-divide by zero error will come
         String operator = operators[random.nextInt(4)];
         textView2.setText(operand1 + operator + operand2);
 
       // Your code here, to diplay correct and incorrect options on the buttons
+        int correctanswer=-100;
+                if (operator.equals("+")) {
+                            correctanswer=operand1+operand2;}
+                else if(operator.equals("-")){
+                    correctanswer=operand1-operand2;}
+                else if(operator.equals("/")){
+                    correctanswer=operand1/operand2;}
+                else if(operator.equals("*")){
+                    correctanswer=operand1*operand2;}
+                if(correctButton==0){
+                    button1.setText(correctanswer+"");
+                    button2.setText(correctanswer+1+"");
+                    button3.setText(correctanswer-1+"");
+                    button4.setText(correctanswer+2+"");
+                }
+                else if(correctButton==1){
+                    button1.setText(correctanswer+1+"");
+                    button2.setText(correctanswer+"");
+                    button3.setText(correctanswer-1+"");
+                    button4.setText(correctanswer+2+"");
+                }
+                else if(correctButton==3){
+                    button1.setText(correctanswer+1+"");
+                    button2.setText(correctanswer-1+"");
+                    button3.setText(correctanswer+"");
+                    button4.setText(correctanswer+2+"");
+                }
+                else if(correctButton==4){
+                    button1.setText(correctanswer+1+"");
+                    button2.setText(correctanswer+2+"");
+                    button3.setText(correctanswer-1+"");
+                    button4.setText(correctanswer+"");
+                }
+
+
+
 
         if(matchCounter==3){    // if three matches are completed updatee the perfomrance in sharedpreferences
 
@@ -95,6 +133,10 @@ public class MainActivity extends AppCompatActivity {
     public int sumOfScore(){
         //Computing the sum of score array, which has the 1 or in each index,depending on correct or incorrect answers
         int sum=0;
+        for(int i=0;i<score.length;i++)
+        {
+            sum=sum+score[i];
+        }
        // your code here
         return sum;
     }
@@ -113,8 +155,21 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public String getInterpretation(int [][]dataFrame,double slope){
+        String interpretation =" ";
+        if(slope>0 && slope<.5)
+            interpretation ="you are a good learner ";
+        else if(slope>.5)
+            interpretation ="you are a damn good learner ";
+        else if (slope<0)
+            interpretation ="you are a unlearner ";
+        else if ( dataFrame[0][1]==3 && slope==0)
+            interpretation = "you achieved perfection";
+        else if (dataFrame[0][1]==0 && slope==0)
+            interpretation = "you do not learned";
+
+
        //provide interpretation based on your slope analysis
         // Your code here
-        return "Your Interpretation";
+        return interpretation;
     }
 }
